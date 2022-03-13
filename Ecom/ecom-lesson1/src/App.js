cf4ac098d7b0316a2fc15f001a92d69f783d4f72
Cruzzhere <stopmenot.2126@gmail.com>
2022-03-14 00:49:00 +0530

    update menu items to navigate withRouter HOC

--- Ecom/ecom-lesson1/src/App.js
+++ Ecom/ecom-lesson1/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
+import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+  
 
 function App() {
   return (
     <div >
-      <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+        <Route exact path='/hats' component={HatsPage} />
+        </Switch>
     </div>
   );
 }
